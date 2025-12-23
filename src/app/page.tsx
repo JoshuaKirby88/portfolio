38bc41f6a679568756a06d37062dd930cbf21d26
Joshua Kirby <jojokirby88@gmail.com>
2025-12-23 16:27:23 +0000

    Added bento grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-import { ComponentExample } from "@/components/component-example"
+import { BentoGrid } from "@/app/_components/bento-grid"
+import { content } from "@/content/content"
 
 export default function Page() {
-	return <ComponentExample />
+	return (
+		<div>
+			<h1 className="mt-20 mb-10 whitespace-pre-wrap text-center font-semibold text-2xl">
+				{content.tagline}
+			</h1>
+
+			<BentoGrid />
+		</div>
+	)
 }
